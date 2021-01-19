d66b4a0a0ac25dcafc941a110d847389bf0074b2
muhammad hanif <moehammadhanif@gmail.com>
2021-01-19 08:31:33 +0700

    update this.js

--- harga-emas/this.js
+++ harga-emas/this.js
@@ -1,53 +1,55 @@
-function rupiah(angka) {
-  /*
-   * Thanks to https://gist.github.com/faisalman/845309#file-convert-rupiah-js
-   */
+document.addEventListener("turbolinks:load", () => {
+  function rupiah(angka) {
+    /*
+     * Thanks to https://gist.github.com/faisalman/845309#file-convert-rupiah-js
+     */
 
-  var rupiah = "";
-  var angkarev = angka.toString().split("").reverse().join("");
+    var rupiah = "";
+    var angkarev = angka.toString().split("").reverse().join("");
 
-  for (var i = 0; i < angkarev.length; i++)
-    if (i % 3 == 0) rupiah += angkarev.substr(i, 3) + ".";
+    for (var i = 0; i < angkarev.length; i++)
+      if (i % 3 == 0) rupiah += angkarev.substr(i, 3) + ".";
 
-  return (
-    "Rp " +
-    rupiah
-      .split("", rupiah.length - 1)
-      .reverse()
-      .join("")
-  );
-}
+    return (
+      "Rp " +
+      rupiah
+        .split("", rupiah.length - 1)
+        .reverse()
+        .join("")
+    );
+  }
 
-function per_gram(harga) {
-  //  1 troy ounce = 31.1034768 gram.
-  return rupiah(((1 / 31.1034768) * harga).toFixed(0));
-}
+  function per_gram(harga) {
+    //  1 troy ounce = 31.1034768 gram.
+    return rupiah(((1 / 31.1034768) * harga).toFixed(0));
+  }
 
-new Vue({
-  el: "#harga-emas",
-  data: {
-    emas: 0,
-  },
-
-  methods: {
-    Harga: async function () {
-      await axios
-        .get("https://data-asg.goldprice.org/dbXRates/IDR")
-        .then((json) => [(this.emas = per_gram(json.data.items[0].xauPrice))])
-        .catch((error) => {
-          (this.emas = "-"), console.log(error);
-        });
+  new Vue({
+    el: "#harga-emas",
+    data: {
+      emas: 0,
     },
 
-    intervalFetchData: function () {
-      setInterval(() => {
-        this.Harga();
-      }, 10000);
-    },
-  },
+    methods: {
+      Harga: async function () {
+        await axios
+          .get("https://data-asg.goldprice.org/dbXRates/IDR")
+          .then((json) => [(this.emas = per_gram(json.data.items[0].xauPrice))])
+          .catch((error) => {
+            (this.emas = "-"), console.log(error);
+          });
+      },
 
-  mounted() {
-    this.Harga();
-    this.intervalFetchData();
-  },
+      intervalFetchData: function () {
+        setInterval(() => {
+          this.Harga();
+        }, 10000);
+      },
+    },
+
+    mounted() {
+      this.Harga();
+      this.intervalFetchData();
+    },
+  });
 });
--- harga-perak/this.js
+++ harga-perak/this.js
@@ -1,53 +1,57 @@
-function rupiah(angka) {
-  /*
-   * Thanks to https://gist.github.com/faisalman/845309#file-convert-rupiah-js
-   */
+document.addEventListener("turbolinks:load", () => {
+  function rupiah(angka) {
+    /*
+     * Thanks to https://gist.github.com/faisalman/845309#file-convert-rupiah-js
+     */
 
-  var rupiah = "";
-  var angkarev = angka.toString().split("").reverse().join("");
+    var rupiah = "";
+    var angkarev = angka.toString().split("").reverse().join("");
 
-  for (var i = 0; i < angkarev.length; i++)
-    if (i % 3 == 0) rupiah += angkarev.substr(i, 3) + ".";
+    for (var i = 0; i < angkarev.length; i++)
+      if (i % 3 == 0) rupiah += angkarev.substr(i, 3) + ".";
 
-  return (
-    "Rp " +
-    rupiah
-      .split("", rupiah.length - 1)
-      .reverse()
-      .join("")
-  );
-}
+    return (
+      "Rp " +
+      rupiah
+        .split("", rupiah.length - 1)
+        .reverse()
+        .join("")
+    );
+  }
 
-function per_gram(harga) {
-  //  1 troy ounce = 31.1034768 gram.
-  return rupiah(((1 / 31.1034768) * harga).toFixed(0));
-}
+  function per_gram(harga) {
+    //  1 troy ounce = 31.1034768 gram.
+    return rupiah(((1 / 31.1034768) * harga).toFixed(0));
+  }
 
-new Vue({
-  el: "#harga-perak",
-  data: {
-    perak: 0,
-  },
-
-  methods: {
-    Harga: async function () {
-      await axios
-        .get("https://data-asg.goldprice.org/dbXRates/IDR")
-        .then((json) => [(this.perak = per_gram(json.data.items[0].xagPrice))])
-        .catch((error) => {
-          (this.perak = "-"), console.log(error);
-        });
+  new Vue({
+    el: "#harga-perak",
+    data: {
+      perak: 0,
     },
 
-    intervalFetchData: function () {
-      setInterval(() => {
-        this.Harga();
-      }, 10000);
-    },
-  },
+    methods: {
+      Harga: async function () {
+        await axios
+          .get("https://data-asg.goldprice.org/dbXRates/IDR")
+          .then((json) => [
+            (this.perak = per_gram(json.data.items[0].xagPrice)),
+          ])
+          .catch((error) => {
+            (this.perak = "-"), console.log(error);
+          });
+      },
 
-  mounted() {
-    this.Harga();
-    this.intervalFetchData();
-  },
+      intervalFetchData: function () {
+        setInterval(() => {
+          this.Harga();
+        }, 10000);
+      },
+    },
+
+    mounted() {
+      this.Harga();
+      this.intervalFetchData();
+    },
+  });
 });
